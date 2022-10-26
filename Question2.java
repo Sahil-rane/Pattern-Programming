183dbaa102283e374fc256901fa4fc72f9b4bbbf
Sahil-rane <86098654+Sahil-rane@users.noreply.github.com>
2022-10-26 23:09:42 +0530

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,24 @@
+package Assignment1;
+
+public class Question2 {
+	
+public static void main(String[] args) {
+		
+		int n=4;
+		int i,j;
+		
+		for(i=1 ; i<=n ; i++)
+		{
+			for(j=1 ; j<=n ; j++) 
+			{
+				System.out.print(i);
+			}
+			
+			
+			System.out.println(" ");
+		}
+
+	}
+
+}
+  
